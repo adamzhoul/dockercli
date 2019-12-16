e2f01e9fb549b3081f53320fd3a8aec25df1c699
adamzhoul <ljylei@163.com>
2019-12-16 10:40:49 +0000

    update

--- pkg/kubernetes/pod.go
+++ pkg/kubernetes/pod.go
@@ -1,12 +1,25 @@
 package kubernetes
 
-// find pod info , node ip included
-func FindPod(podName string) {
+import (
+	"fmt"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// find pods info , node ip included
+func FindPod(podsName []string) {
 
 	if k8sClient == nil {
 		return
 	}
 
 	// k8sClient.AppsV1().RESTClient().Get()
+	pods, err := k8sClient.CoreV1().Pods("namespace").List(metav1.ListOptions{})
+	if err != nil {
+		// handle error
+	}
+	for _, pod := range pods.Items {
+		fmt.Println(pod.Name, pod.Status.PodIP)
+	}
 
 }
